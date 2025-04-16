870d2480ed4bb01af6421d9269710672b05b2e63
Alberto Basalo <albertobasalo@hotmail.com>
2025-04-16 13:16:56 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
 /**
  * ArchetypeNodeCLI main entry point
  */
-import { processCommand } from "./app/sample.command.ts";
-import { logError, logWarn } from "./system/log.utils.ts";
-
-processCommand()
-	.then((res) => {
-		logWarn("Command processed successfully.", res);
-	})
-	.catch((error) => {
-		logError("Error processing command. ", error);
-	});
+console.log("ArchetypeNodeCLI is running...");
